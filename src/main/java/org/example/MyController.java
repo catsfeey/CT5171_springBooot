95940a8b4f7440616271ff6a34a576a94dc60528
catsfeey <catrionafeehily@outlook.com>
2023-11-03 13:30:08 +0800

    Initial commit

---
+++ src/main/java/org/example/MyController.java
@@ -0,0 +1,19 @@
+package org.example;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public String index() {
+
+        //return "Greetings from Spring Boot!";
+        LocalDate fromDate = LocalDate.now();
+        LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
+        return "There are " + Days.daysBetween(fromDate, newYear) + "days until the new year!";
+    }
+
+}
+
